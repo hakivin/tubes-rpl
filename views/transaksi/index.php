bd63e4edd1a6233532765ca7f203bfc9092f5829
Bimo <adrianbimo4@gmail.com>
2019-06-26 18:15:26 +0700

    update

--- views/transaksi/index.php
+++ views/transaksi/index.php
@@ -20,14 +20,14 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            // ['class' => 'yii\grid\SerialColumn'],
 
             'id_transaksi',
             'waktu_transaksi',
             'id_member',
             'id_petugas',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            // ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 
