c20394c4ee76454adeb72d1fbb2f854b3825e494
Mahas135 <48470908+Mahas135@users.noreply.github.com>
2019-06-26 23:30:42 +0700

    Update index.php

--- views/keuangan/index.php
+++ views/keuangan/index.php
@@ -12,15 +12,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 	// Perform queries 
-	$sql = "Select sum((select harga_jual from obat where kode_obat = transaksi.kode_obat) * jumlah_beli), DATE(transaksi.waktu_transaksi), Sum(harga_beli),
-	Sum(harga_jual * jumlah_beli) - Sum(harga_beli)	from transaksi, obat group by DATE(transaksi.waktu_transaksi)";
+	$sql = "Select * from keuangan";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		// output data of each row
-		echo "Waktu" . "Omset" . " Harga Beli "." Keuntungan "."<br>";
+		echo " Waktu " . " Omset " . " Harga Beli "." Keuntungan "."<br>";
 		while($row = $result->fetch_assoc()) {
-			echo " " .$row["DATE(transaksi.waktu_transaksi)"]." ".$row["sum((select harga_jual from obat where kode_obat = transaksi.kode_obat) * jumlah_beli)"]." " 
-			.$row["Sum(harga_beli)"]." " .$row["Sum(harga_jual * jumlah_beli) - Sum(harga_beli)"]."<br>";
+			echo " " .$row["tanggal"]." ".$row["omzet"]." " .$row["harga_pokok"]." " .$row["laba"]."<br>";
 		}
 		
 	} else {
