e19b15058f275c9a6e3459b7137869f4d94f2e21
Mahas135 <48470908+Mahas135@users.noreply.github.com>
2019-06-27 08:10:50 +0700

    Update index.php

--- views/keuangan/index.php
+++ views/keuangan/index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -12,18 +23,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 	// Perform queries 
-	$sql = "Select * from keuangan";
+	$sql = "Select sum(harga_jual * jumlah_beli), DATE(transaksi.waktu_transaksi), Sum(harga_beli),
+	Sum(harga_jual * jumlah_beli) - Sum(harga_beli)	from transaksi, obat group by DATE(transaksi.waktu_transaksi)";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		// output data of each row
-		echo " Waktu " . " Omset " . " Harga Beli "." Keuntungan "."<br>";
+		echo "<table style='width:100%;background-color:#00cbff;'><tr><th>Waktu</th><th>Omset</th><th>Harga Beli</th><th>Keuntungan</th></tr>";
 		while($row = $result->fetch_assoc()) {
-			echo " " .$row["tanggal"]." ".$row["omzet"]." " .$row["harga_pokok"]." " .$row["laba"]."<br>";
+			echo "<tr><>" .$row["DATE(transaksi.waktu_transaksi)"]."</td><td>".$row["sum(harga_jual * jumlah_beli)"]."</td><td>" 
+			.$row["Sum(harga_beli)"]."</td><td>" .$row["Sum(harga_jual * jumlah_beli) - Sum(harga_beli)"]."</td></tr>";
 		}
-		
+		echo "</table>";
 	} else {
 		echo "0 results";
 	}
 	
 	$conn->close();
 ?>
+
+</body>
+</html>
