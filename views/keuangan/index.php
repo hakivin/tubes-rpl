91203f5ed10d301fdef32acfc32b0f535da46969
hakivin <hakivinamana@student.uns.ac.id>
2019-06-27 08:21:42 +0700

    add keuangan

--- views/keuangan/index.php
+++ views/keuangan/index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -24,4 +24,40 @@ if ($conn->connect_error) {
 	}
 	
 	$conn->close();
+?> -->
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\TransaksiSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Keuangan';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="keuangan-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+  
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'tanggal',
+            'omzet',
+            'harga_pokok',
+            'laba',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
